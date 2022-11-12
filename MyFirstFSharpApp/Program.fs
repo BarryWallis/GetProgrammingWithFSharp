23ed947397d5d4a372b4039c7d71bded16c81258
Barry Wallis <barry.wallis@outlook.com>
2022-11-12 12:51:33 -0800

    Add project files.

---
+++ MyFirstFSharpApp/Program.fs
@@ -0,0 +1,6 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+[<EntryPoint>]
+let main argv = 
+    let items = argv.Length
+    printfn "Passed in %d items: %A" items argv
+    0
